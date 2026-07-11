2f0d49f6b1186917aef6e47a2cec061815931088
= <=>
2026-07-11 18:08:34 -0500

    ultimo cambio

--- src/main/java/pe/cibertec/entity/Usuario.java
+++ src/main/java/pe/cibertec/entity/Usuario.java
@@ -1,14 +1,19 @@
 package pe.cibertec.entity;
 
-
-
+import jakarta.persistence.*;
+import lombok.Data;
 
 @Data
 @Entity
 @Table(name = "usuario")
 public class Usuario {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
+    @Column(nullable = false)
     private String nombre;
+    @Column(nullable = false)
     private String correo;
+    @Column(nullable = false)
     private String clave;
 }

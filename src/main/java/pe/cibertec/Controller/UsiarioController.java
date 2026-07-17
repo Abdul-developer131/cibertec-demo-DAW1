202090e14dda33250a3e4a89c3e49523937683e4
= <=>
2026-07-16 19:39:53 -0500

    Creando el Controller

---
+++ src/main/java/pe/cibertec/Controller/UsiarioController.java
@@ -0,0 +1,37 @@
+package pe.cibertec.Controller;
+
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import pe.cibertec.entity.Usuario;
+import pe.cibertec.repository.UsuarioRepository;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/usuarios")
+public class UsiarioController {
+    private final UsuarioRepository usuarioRepository;
+
+    //Constructur para inicializar repository
+    public UsiarioController(UsuarioRepository usuarioRepository) {
+        this.usuarioRepository = usuarioRepository;
+    }
+
+    //Funcion para registrar un nuevo usario
+    @PostMapping("/registrar")
+    //ResponseEntity es la respuesta que guarda el registro
+    public ResponseEntity<Usuario> registrar(Usuario usuario, UsuarioRepository usuarioRepository){
+        Usuario usuarioRegistrado = usuarioRepository.save(usuario);
+        return ResponseEntity.ok(usuarioRegistrado);
+    }
+
+    // Funcion para mopstrar todos los usuarios registrados
+    @GetMapping
+    public List<Usuario> listar(UsuarioRepository usuarioRepository){
+        return usuarioRepository.findAll();
+    }
+}
